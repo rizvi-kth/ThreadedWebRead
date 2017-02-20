415548c3179f760cc270313fd19b514560d09552
rizvi-kth <rizvi.kth@gmail.com>
2017-02-20 12:46:22 +0100

    With WPF app

---
+++ ThreadedWebReadWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadedWebReadWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private WebClient wc = new WebClient();
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = false;
+            Task<string> tsk = wc.DownloadStringTaskAsync("http://localhost:9240/Slow.ashx");
+
+            // FromCurrentSynchronizationContext() returns backs the context thread from where main thread can be returned.
+            TaskScheduler taskSchedular = TaskScheduler.FromCurrentSynchronizationContext();
+
+            tsk.ContinueWith(t =>
+            {
+                // At this point we are not on main thread which is controlling the WPF-UI; we are on a worker thread.
+                textBlock.Text = t.Result;
+                button.IsEnabled = true;
+            }, taskSchedular);
+
+        }
+    }
+}
